5fe091725620e1e443681031e78fee8b9acc64de
alexisi-w <alexis@i-w.mx>
2026-09-22 14:24:41 -0600

    Update sheetId in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 {
   "appsScriptId": "REEMPLAZAR_CON_TU_SCRIPT_ID",
-  "sheetId": "1uCqsdU9EITgU7LMHPApAfVWkmN4tMY4g1U2mrwOKZPU",
+  "sheetId": "AKfycbwcf61XQ3iC2ccIcq36WZs_N3up-nblOi1S1w2hvuSc4fA6EL3EUU54zvRyrDp7mTwG",
   "environment": "production",
   "version": "1.0.0"
 }
