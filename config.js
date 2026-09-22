7f0a9802236b6ccb347b0bf66daa121b27e024f7
alexisi-w <alexis@i-w.mx>
2026-09-22 14:34:31 -0600

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-{
-  "appsScriptId": "1w89wEt6M0AvISnAdm7d9T3xO8ATq2mXqdz1Foo_7I-MkC4hE1ED-hWeV",
-  "sheetId": "1uCqsdU9EITgU7LMHPApAfVWkmN4tMY4g1U2mrwOKZPU",
-  "environment": "production",
-  "version": "1.0.0"
-}
