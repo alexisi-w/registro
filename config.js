7289c9e2cc973d089ae5f2382a1c5cb6d8bf1dd6
alexisi-w <alexis@i-w.mx>
2026-09-22 10:56:20 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-// config.js en GitHub
+// ====== CONFIGURACIÓN DEPLOY ID ======
+// Actualizar este archivo cada vez que hagas un nuevo Deploy en Apps Script
+// Los 16 usuarios siempre acceden a: https://alexisi-w.github.io/registro/
+// Solo cambia el deployId aquí cuando despliegues
+
 const DEPLOY_CONFIG = {
   deployId: 'AKfycbyFBGiJM0-HlyrNW9f41AUFEa-28McgCqLid_svXSfSmoLKL19NpbGOSwTAEztG-ds'
 };
